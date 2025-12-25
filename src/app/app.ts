b37d335383d8cbe07ace2143c755f03bf75891a2
Aya Adel <ayaadelyahoo66@gmail.com>
2025-12-25 20:54:33 +0200

    Add component Footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,10 +5,11 @@ import { initFlowbite } from 'flowbite';
 import { Navbar } from './core/components/navbar/navbar';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { ProductPageComponent } from './feature/products/pages/product-page/product-page.component';
+import { Footer } from './core/components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, ProductPageComponent, NgxSpinnerModule],
+  imports: [RouterOutlet, Navbar, ProductPageComponent, NgxSpinnerModule, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- src/app/core/components/footer/footer.ts
+++ src/app/core/components/footer/footer.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CategoriesService } from '../../../feature/categories/services/categories.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {
-
+export class Footer implements OnInit {
+  public readonly categoriesService = inject(CategoriesService);
+  ngOnInit(): void {
+    this.categoriesService.getAllCategories();
+  }
 }
