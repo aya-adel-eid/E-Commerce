071d059ef6ac69ab32534c3992e6076f0a9dd81a
Aya ِAdel <ayaadelyahoo66@gmail.com>
2026-08-11 19:21:33 +0300

    edit in vercel.json

--- api/index.js
+++ api/index.js
@@ -1,4 +1,7 @@
 export default async (req, res) => {
+  if (!req.headers.host) {
+    req.headers.host = req.headers['x-forwarded-host'] || 'e-commerce-alpha-ten-99.vercel.app';
+  }
   const { reqHandler } = await import('../dist/E-commerce01/server/server.mjs');
   return reqHandler(req, res);
 };
